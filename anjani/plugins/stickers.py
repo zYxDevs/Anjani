02d6285e08345e6d86fc4ae2c7a09975b619ac44
mrmissx <hi@mrmiss.me>
2022-04-01 18:07:44 +0700

    *1 Refactor
    - Fix importing a non exported features from module
    - Split (create, add and upload) pyrogram raw function
    - Use AsyncPath
    - Split resize method based on the media context
    - Fix several typings
    - Use staticmethod on non used self method
    - Create pack by the invoker peer instead bot owner
    
    * Suppress linter for pyrogram raw function

--- anjani/plugins/stickers.py
+++ anjani/plugins/stickers.py
@@ -1,4 +1,4 @@
-"""stickers bot commands"""
+"""stickers commands"""
 # Copyright (C) 2020 - 2022  UserbotIndo Team, <https://github.com/userbotindo.git>
 #
 # This program is free software: you can redistribute it and/or modify
@@ -15,249 +15,246 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
-from typing import ClassVar
+import shlex
+from typing import Any, ClassVar, Optional
 
-import pyrogram
+from aiopath import AsyncPath
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 from PIL import Image
+from pyrogram import emoji, raw
 from pyrogram.errors import StickersetInvalid
-from pyrogram.raw.functions.messages import GetStickerSet, UploadMedia
-from pyrogram.raw.functions.stickers import AddStickerToSet, CreateStickerSet
-from pyrogram.raw.types import (
-    DocumentAttributeFilename,
-    InputDocument,
-    InputMediaUploadedDocument,
-    InputStickerSetItem,
-    InputStickerSetShortName,
-)
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
 from anjani import command, plugin, util
 
 
-class Stickers(plugin.Plugin):
-    """Plugin init, sticker."""
-
-    name: ClassVar[str] = 'Stickers'
+class Sticker(plugin.Plugin):
+    name: ClassVar[str] = "Stickers"
     helpable: ClassVar[bool] = True
 
-    async def _resize(self, media: str, video: bool) -> str:  # pylint: disable=no-self-use
-        """Resize the given media to 512x512"""
-        if video:
-            metadata = extractMetadata(createParser(media))
-            width = round(metadata.get('width', 512))
-            height = round(metadata.get('height', 512))
-            if height == width:
-                height, width = 512, 512
-            elif height > width:
-                height, width = 512, -1
-            elif width > height:
-                height, width = -1, 512
+    @staticmethod
+    async def resize_video(media: AsyncPath):
+        metadata = extractMetadata(createParser(str(media)))
+        width = round(metadata.get("width", 512))  # type: ignore
+        height = round(metadata.get("height", 512))  # type: ignore
+        if height == width:
+            height, width = 512, 512
+        elif height > width:
+            height, width = 512, -1
+        elif width < width:
+            height, width = -1, 512
 
-            _video = f'{media}.webm'
-            arg = (
-                f'ffmpeg -i {media} -ss 00:00:00 -to 00:00:03 -map 0:v -b 256k -fs 262144'
-                + f' -c:v libvpx-vp9 -vf scale={width}:{height},fps=30 {_video} -y'
+        video = AsyncPath("downloads/sickers.webm")
+        arg = (
+            f"ffmpeg -i {media} -ss 00:00:00 -to 00:00:03 -map 0:v -b 256k -fs 262144"
+            + f" -c:v libvpx-vp9 -vf scale={width}:{height},fps=30 {video} -y"
+        )
+        await util.system.run_command(*shlex.split(arg))
+        await media.unlink()
+        return video
+
+    @staticmethod
+    async def resize_image(image: AsyncPath):
+        img = Image.open(str(image))
+        scale = 512 / max(img.width, img.height)
+        new_size = (int(img.width * scale), int(img.height * scale))
+        img = img.resize(new_size, Image.LANCZOS)
+        image_path = AsyncPath("downloads/sticker.png")
+        img.save(image_path, "PNG")
+        await image.unlink()
+        return image_path
+
+    async def _upload_media(
+        self, sticker: str
+    ) -> raw.types.message_media_document.MessageMediaDocument:
+        return await self.bot.client.send(
+            raw.functions.messages.upload_media.UploadMedia(
+                peer=await self.bot.client.resolve_peer("stickers"),  # type: ignore
+                media=raw.types.input_media_uploaded_document.InputMediaUploadedDocument(
+                    mime_type=self.bot.client.guess_mime_type(sticker) or "application/zip",
+                    file=await self.bot.client.save_file(sticker),  # type: ignore
+                    force_file=True,
+                    thumb=None,  # type: ignore
+                    attributes=[
+                        raw.types.document_attribute_filename.DocumentAttributeFilename(
+                            file_name=os.path.basename(sticker)
+                        )
+                    ],  # type: ignore
+                ),
             )
-            await util.system.run_command(arg, shell=True)   # skipcq: BAN-B604
-            os.remove(media)
-            return _video
+        )
 
-        image = Image.open(media)
-        scale = 512 / max(image.width, image.height)
-        new_size = (int(image.width * scale), int(image.height * scale))
-        image = image.resize(new_size, Image.LANCZOS)
-        _photo = 'sticker.png'
-        image.save(_photo, 'PNG')
-        os.remove(media)
-        return _photo
-
-    async def create_pack(
-        self, pack_name: str, short_name: str, sticker: str, emoji: str, set_type: str
-    ) -> bool:
-        """Sticker pack creator, Return: True if success."""
-        media = (
-            await self.bot.client.send(
-                UploadMedia(
-                    peer=await self.bot.client.resolve_peer('stickers'),
-                    media=InputMediaUploadedDocument(
-                        mime_type=self.bot.client.guess_mime_type(sticker) or 'application/zip',
-                        file=(await self.bot.client.save_file(sticker)),
-                        force_file=True,
-                        thumb=None,
-                        attributes=[DocumentAttributeFilename(file_name=os.path.basename(sticker))],
-                    ),
-                )
-            )
-        ).document
-        await self.bot.client.send(
-            CreateStickerSet(
-                user_id=await self.bot.client.resolve_peer(self.bot.config['owner_id']),
+    async def _create_pack(
+        self, author: int, pack_name: str, short_name: str, media: Any, emoji: str, set_type: str
+    ):
+        media = await self._upload_media(media)
+        return await self.bot.client.send(
+            raw.functions.stickers.create_sticker_set.CreateStickerSet(
+                user_id=await self.bot.client.resolve_peer(author),  # type: ignore
                 title=pack_name,
                 short_name=short_name,
                 stickers=[
-                    InputStickerSetItem(
-                        document=InputDocument(
-                            id=media.id,
-                            access_hash=media.access_hash,
-                            file_reference=media.file_reference,
-                        ),
+                    raw.types.input_sticker_set_item.InputStickerSetItem(
+                        document=raw.types.input_document.InputDocument(
+                            id=media.document.id,
+                            access_hash=media.document.access_hash,
+                            file_reference=media.document.file_reference,
+                        ),  # type: ignore
                         emoji=emoji,
                     )
                 ],
-                animated=set_type == 'anim',
-                videos=set_type == 'vid',
+                animated=set_type == "anim",
+                videos=set_type == "vid",
             )
         )
-        return True
 
-    async def add_sticker(self, short_name: str, sticker: str, emoji: str) -> bool:
-        """Add file to sticker, Return true if success."""
-        media = (
-            await self.bot.client.send(
-                UploadMedia(
-                    peer=await self.bot.client.resolve_peer('stickers'),
-                    media=InputMediaUploadedDocument(
-                        mime_type=self.bot.client.guess_mime_type(sticker) or 'application/zip',
-                        file=(await self.bot.client.save_file(sticker)),
-                        force_file=True,
-                        thumb=None,
-                        attributes=[DocumentAttributeFilename(file_name=os.path.basename(sticker))],
-                    ),
-                )
-            )
-        ).document
-        await self.bot.client.send(
-            AddStickerToSet(
-                stickerset=InputStickerSetShortName(short_name=short_name),
-                sticker=InputStickerSetItem(
-                    document=InputDocument(
-                        id=media.id,
-                        access_hash=media.access_hash,
-                        file_reference=media.file_reference,
-                    ),
+    async def _add_sticker(self, short_name: str, media: Any, emoji: str):
+        media = await self._upload_media(media)
+        return await self.bot.client.send(
+            raw.functions.stickers.add_sticker_to_set.AddStickerToSet(
+                stickerset=raw.types.input_sticker_set_short_name.InputStickerSetShortName(
+                    short_name=short_name
+                ),  # type: ignore
+                sticker=raw.types.input_sticker_set_item.InputStickerSetItem(
+                    document=raw.types.input_document.InputDocument(
+                        id=media.document.id,
+                        access_hash=media.document.access_hash,
+                        file_reference=media.document.file_reference,
+                    ),  # type: ignore
                     emoji=emoji,
                 ),
             )
         )
-        return True
 
-    async def cmd_kang(self, ctx: command.Context) -> str:
-        """Kang sticker handler."""
+    async def cmd_kang(self, ctx: command.Context) -> Optional[str]:
         chat = ctx.msg.chat
         reply = ctx.msg.reply_to_message
 
         if not reply or not reply.media:
-            return await self.text(chat.id, 'sticker-no-reply')
+            return await self.text(chat.id, "sticker-no-reply")
 
-        video_setpack: bool = False
-        resize: bool = False
-        await ctx.respond(await self.text(chat.id, 'sticker-kang-process'))
-        anim_setpack: bool = False
-        set_emoji: str = ''
-        if reply.photo or reply.document and 'image' in reply.document.mime_type:
-            resize: bool = True
+        await ctx.respond(await self.text(chat.id, "sticker-kang-process"))
+        resize = False
+        anim_setpack = False
+        video_setpack = False
+        set_emoji = ""
+        if reply.photo or reply.document and "image" in reply.document.mime_type:
+            resize = True
         elif reply.animation or (
             reply.document
-            and 'video' in reply.document.mime_type
+            and "video" in reply.document.mime_type
             and reply.document.file_size <= 10485760
         ):
-            video_setpack: bool = True
-            resize: bool = True
-        elif reply.document and 'tgsticker' in reply.document.mime_type:
-            anim_setpack: bool = True
+            video_setpack = True
+            resize = True
         elif reply.sticker:
             if reply.sticker.file_name is None:
-                return self.text(chat.id, 'sticker-filename-missing')
-            has_emoji = reply.sticker.emoji
-            if has_emoji:
-                set_emoji = has_emoji
+                return await self.text(chat.id, "sticker-filename-missing")
+            if reply.sticker.emoji:
+                set_emoji = reply.sticker.emoji
             video_setpack = reply.sticker.is_video
             anim_setpack = reply.sticker.is_animated
-            if not reply.sticker.file_name.endswith('tgs') or reply.sticker.file_name.endswith(
-                '.webm'
-            ):
-                resize: bool = True
+            sticker_name = reply.sticker.file_name
+            if not sticker_name.endswith("tgs") or sticker_name.endswith(".webm"):
+                resize = True
         else:
-            return await self.text(chat.id, 'sticker-unsupported-file')
-        media = await reply.download()
-        if not media:
-            return await self.text(chat.id, 'sticker-media-notfound')
+            return await self.text(chat.id, "sticker-unsupported-file")
 
-        args = ctx.args
-        packnum: int = 1
-        emojiset = None
-        if len(args) == 2:
-            emojiset, packnum = args
-        elif len(args) == 1:
+        media = AsyncPath(await reply.download())
+        if not media:
+            return await self.text(chat.id, "sticker-media-notfound")
+
+        packnum = 1
+        emojiset = ""
+        if len(ctx.args) == 2:
+            emojiset, packnum = ctx.args
+        elif len(ctx.args) == 1:
             if ctx.input[0].isnumeric():
-                packnum: int = ctx.input[0]
+                packnum = ctx.args[0]
             else:
                 emojiset = ctx.input[0]
 
-        if emojiset is not None:
+        if emojiset:
             setas = set_emoji
             for i in emojiset:
-                if i and i in (
-                    getattr(pyrogram.emoji, e) for e in dir(pyrogram.emoji) if not e.startswith('_')
-                ):
+                if i and i in (getattr(emoji, e) for e in dir(emoji) if not e.startswith("_")):
                     set_emoji += i
                 if setas and setas != set_emoji:
                     set_emoji = set_emoji[len(setas) :]
         if not emojiset:
-            set_emoji = '🤔'
-        authname = ctx.author.username or ctx.author.id
-        packname: str = f'a{ctx.author.id}_Anjani_{packnum}'
-        packnick: str = f"{authname}'s Kang Pack Vol.{packnum}"
+            set_emoji = "🤔"
+
+        packnum = int(packnum)
+
+        author = ctx.author
+        if not author:  # sanity check
+            return
+
+        author_id = author.id
+        author_name = author.username or author.first_name
+        pack_name = f"a{author_id}_Anjani_{packnum}"
+        pack_nick = f"{author_name}'s Kang Pack Vol.{packnum}"
 
         if resize:
-            media = await self._resize(media, video_setpack)
+            if video_setpack:
+                media = await self.resize_video(media)
+            else:
+                media = await self.resize_image(media)
         if anim_setpack:
-            packname += '_anim'
-            packnick += ' (Animated)'
+            pack_name += "_anim"
+            pack_nick += " (Animated)"
         if video_setpack:
-            packname += '_video'
-            packnick += ' (Video)'
-        hasexist: bool = False
+            pack_name += "_video"
+            pack_nick += " (Video)"
+
+        pack_name += f"_by_{self.bot.user.username}"
+        exist = False
         while True:
-            packname += f'_by_{self.bot.user.username}'
             try:
-                hasexist = await self.bot.client.send(
-                    GetStickerSet(stickerset=InputStickerSetShortName(short_name=packname), hash=0)
+                exist_pack = await self.bot.client.send(
+                    raw.functions.messages.get_sticker_set.GetStickerSet(
+                        stickerset=raw.types.input_sticker_set_short_name.InputStickerSetShortName(
+                            short_name=pack_name,
+                        ),  # type: ignore
+                        hash=0,
+                    )
                 )
             except StickersetInvalid:
-                hasexist: bool = False
+                exist = False
                 break
             else:
+                exist = True
                 pack_limit = 50 if (anim_setpack or video_setpack) else 120
-                if hasexist.set.count >= pack_limit:
+                if exist_pack.set.count >= pack_limit:
                     packnum += 1
-                    packname: str = f'a{ctx.author.id}_Anjani_{packnum}'
-                    packnick: str = f"{authname}'s Kang Pack Vol.{packnum}"
+                    pack_name = f"a{author_id}_Anjani_{packnum}"
+                    pack_nick = f"{author_name}'s Kang Pack Vol.{packnum}"
                     if anim_setpack:
-                        packname += '_anim'
-                        packnick += ' (Animated)'
+                        pack_name += "_anim"
+                        pack_nick += " (Animated)"
                     if video_setpack:
-                        packname += '_video'
-                        packnick += ' (Video)'
-                    await ctx.respond(
-                        await self.text(chat.id, 'sticker-pack-insufficient', packnum)
-                    )
-                    packname += f'_by_{self.bot.user.username}'
+                        pack_name += "_video"
+                        pack_nick += " (Video)"
+                    await ctx.respond(await self.text(chat.id, "sticker-pack-insufficent", packnum))
+                    pack_name += f"_by_{self.bot.user.username}"
                     continue
                 break
-        if hasexist:
-            await self.add_sticker(packname, media, set_emoji)
+
+        if exist:
+            await self._add_sticker(pack_name, str(media), set_emoji)
         else:
-            set_type = 'anim' if anim_setpack else 'vid' if video_setpack else 'static'
-            await ctx.respond(await self.text(chat.id, 'sticker-new-pack'))
-            await self.create_pack(packnick, packname, media, set_emoji, set_type)
+            set_type = "anim" if anim_setpack else "vid" if video_setpack else "static"
+            await ctx.respond(await self.text(chat.id, "sticker-new-pack"))
+            await self._create_pack(
+                author_id, pack_nick, pack_name, str(media), set_emoji, set_type
+            )
+
         keyb = InlineKeyboardButton(
-            text=await self.text(chat.id, 'sticker-pack-btn'), url=f't.me/addstickers/{packname}'
+            text=await self.text(chat.id, "sticker-pack-btn"), url=f"t.me/addstickers/{pack_name}"
         )
         await ctx.respond(
-            await self.text(chat.id, 'sticker-kang-success'),
+            await self.text(chat.id, "sticker-kang-success"),
             reply_markup=InlineKeyboardMarkup([[keyb]]),
         )
-        if os.path.exists(str(media)):
-            return os.remove(media)
+        await media.unlink()
